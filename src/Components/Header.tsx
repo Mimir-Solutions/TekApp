05e630a8039181f3ff68797b5c9a900123967348
Alex Priadko <mr.eskrano@gmail.com>
2021-09-23 10:02:44 +0300

    Week 3

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -21,7 +21,7 @@ export const Header: FunctionComponent<{}> = (props) => {
                             className="d-inline-block align-top"
                             alt="$TEK"
                         />
-                        GET $TEK <span className="text-danger text-small">week 2</span>
+                        GET $TEK <span className="text-danger text-small">week 3</span>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse className="justify-content-end" id="navbarScroll">
